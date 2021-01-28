11cbdf9a760035264e633bca4ac00685cb237713
Denis <dennyjuice@gmail.com>
2021-01-29 01:14:27 +0700

    add nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,12 @@
 import '../styles/_globals.css';
 import type { AppProps /*, AppContext */ } from 'next/app';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function App({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />;
